b3bd7bc48332fae45c94f2016863264ac99aafce
majstyle <majestyirodinjo@gmail.com>
2024-08-22 13:47:09 +0100

    commit7

--- app/Preline/PrelineScript.tsx
+++ app/Preline/PrelineScript.tsx
@@ -17,7 +17,7 @@ export default function PrelineScript() {
     const loadPreline = async () => {
       await import("preline/preline");
 
-      window.HSStaticMethods.autoInit();
+      // window.HSStaticMethods.autoInit();
     };
 
     loadPreline();
