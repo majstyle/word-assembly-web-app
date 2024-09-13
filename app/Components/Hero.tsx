378dd37321ba7f862298414ee8e6fa9c18d57794
majstyle <majestyirodinjo@gmail.com>
2024-09-13 09:56:59 +0100

    Commission

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -2,53 +2,55 @@ export default function Hero() {
   return (
     <>
       {/* Slider */}
-      <div className="bg-hero-pattern bg-cover bg-center w-full h-screen">
-        <h1 className="text-3xl text-left flex items-center flex-col pt-40 text-gray-800 font-bold sm:text-5xl lg:text-7xl lg:leading-tight dark:text-slate-100">
+      <div className="bg-hero-pattern bg-cover bg-center h-96 lg:w-full lg:h-screen text-center leading-3">
+        <h1 className="text-3xl text-center lg:text-center lg:flex lg:items-center lg:flex-col lg:pt-40 pt-10 text-gray-800 font-bold lg:text-7xl lg:leading-tight dark:text-slate-100">
           Welcome To Word Assembly{" "}
-          <span className="text-slate-200 text-2xl">We raise leaders that are spiritually and socially relevant</span>
         </h1>
-        <div className="flex justify-center lg:mt-10">
-        <a
-                    className="py-5 px-6 inline-flex justify-center items-center lg:ml-2 gap-x-0 text-sm font-medium rounded-lg border border-transparent bg-yellow-500 text-gray-800 hover:bg-yellow-600 focus:outline-none focus:bg-red-500 disabled:opacity-50 disabled:pointer-events-none"
-                    href="#"
-                  >
-                    View Locations
-                    <svg
-                      className="shrink-0 size-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="m9 18 6-6-6-6" />
-                    </svg>
-                  </a>
+        <span className="text-slate-200 lg:text-2xl text-sm lg:flex lg:justify-center pt-4 lg:pt-0">
+          We raise leaders that are spiritually and socially relevant
+        </span>
+        <div className="flex flex-col gap-y-5 items-center mt-6 lg:flex-row lg:justify-center lg:gap-x-2 lg:mt-10">
+          <a
+            className="py-3 px-6 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-500 text-gray-800 hover:bg-yellow-600 focus:outline-none focus:bg-red-500 disabled:opacity-50 disabled:pointer-events-none"
+            href="#"
+          >
+            View Locations
+            <svg
+              className="ml-2 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </a>
 
-                  <a
-                    className="py-5 px-6 inline-flex justify-center items-center lg:ml-2 gap-x-0 text-sm font-medium rounded-lg border border-transparent bg-yellow-500 text-gray-800 hover:bg-yellow-600 focus:outline-none focus:bg-red-500 disabled:opacity-50 disabled:pointer-events-none"
-                    href="#"
-                  >
-                    Join online
-                    <svg
-                      className="shrink-0 size-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="m9 18 6-6-6-6" />
-                    </svg>
-                  </a>
+          <a
+            className="py-3 px-6 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-500 text-gray-800 hover:bg-yellow-600 focus:outline-none focus:bg-red-500 disabled:opacity-50 disabled:pointer-events-none"
+            href="#"
+          >
+            Join Online
+            <svg
+              className="ml-2 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </a>
         </div>
       </div>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/preline/preline.js',
-
+    "./node_modules/preline/preline.js",
   ],
   theme: {
     extend: {
@@ -14,13 +13,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "hero-pattern":
+        "hero-pattern":
           "linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/assets/bgg.jpg')",
       },
     },
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
+  plugins: [require("preline/plugin")],
 };
 export default config;
